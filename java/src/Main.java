eee99e437bb504e80c052cb50a659475fe02e510
sarkerrabi <sarkerrabi16@gmail.com>
2018-12-15 19:12:25 +0600

    added comments within the code

--- java/src/Main.java
+++ java/src/Main.java
@@ -11,22 +11,27 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+
+        //given file
         String filepath = "NCHS_-_Leading_Causes_of_Death__United_States.csv";
 
         String inpurText = "";
         Scanner scanner = new Scanner(System.in);
 
+        //take user input
         inpurText = scanner.nextLine();
 
-
+        //read file using file reader
         Reader inputFile  = new FileReader(filepath);
         try {
+
+            //take all data in to a list of records
             Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(inputFile);
             for (CSVRecord record :
                     records) {
 //                String year = record.get(0);
 //                System.out.println(year);
-
+            //check user inputs and data
                 if (record.get(0).toLowerCase().trim().equals(inpurText.toLowerCase().trim())
                         ||record.get(1).toLowerCase().trim().equals(inpurText.toLowerCase().trim())
                         ||record.get(2).toLowerCase().trim().equals(inpurText.toLowerCase().trim())
@@ -34,6 +39,7 @@ public class Main {
                         ||record.get(4).toLowerCase().trim().equals(inpurText.toLowerCase().trim())
                         ||record.get(5).toLowerCase().trim().equals(inpurText.toLowerCase().trim()))
                 {
+                    // pint data into console where user and given data are equal
                     System.out.println(record.get(0)+" "+record.get(1)+" "+record.get(2)
                             +" "+record.get(3)+" "+record.get(4)+" "+record.get(5));
                 }
